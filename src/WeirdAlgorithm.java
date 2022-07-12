6a8f3269312c9fb598282f5d483cdd120db8e124
hongdangcseiu <hongdang2k1@gmail.com>
2022-07-12 11:19:38 +0700

    weird algorithm

--- src/WeirdAlgorithm.java
+++ src/WeirdAlgorithm.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class WeirdAlgorithm {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		long num = scan.nextLong();
+
+		while (num != 1) {
+			System.out.print(num + " ");
+			if (num % 2 == 0) {
+				num = num / 2;
+			} else {
+				num = 3 * num + 1;
+			}
+		}
+		System.out.print(num);
+	}
 
 }
